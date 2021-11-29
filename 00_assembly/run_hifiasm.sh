52d7e5ae566ab5026b053ce36c250990e4841a4e
Tim Sackton <tsackton@oeb.harvard.edu>
2021-11-29 14:16:58 -0500

    updating assembly and qc scripts

--- 00_assembly/run_hifiasm.sh
+++ 00_assembly/run_hifiasm.sh
@@ -5,6 +5,6 @@
 #sample id is first, threads is second
 SAMPLE=$1
 THREADS=24 #fixed to ntasks from slurm script
-SEQFILES="/n/holylfs05/LABS/informatics/Everyone/scrubjay/00_raw_data/$SAMPLE/*/*.fastq.gz"
+SEQFILES="/n/holylfs05/LABS/informatics/Everyone/scrubjay/RAW_DATA/$SAMPLE/*/*.fastq.gz"
 
 ../../hifiasm/hifiasm -t $THREADS -o $SAMPLE $SEQFILES
